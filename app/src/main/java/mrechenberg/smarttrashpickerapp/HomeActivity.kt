74a3d2fe80d158a64d51c0b494f3cf53145013c1
MichaelRechenberg <rchnbrg2@illinois.edu>
2019-05-04 17:18:48 -0500

    Made the LocationRequest request more frequent location updates (following guidelines given in https://developer.android.com/training/location/change-location-settings)

--- app/src/main/java/mrechenberg/smarttrashpickerapp/HomeActivity.kt
+++ app/src/main/java/mrechenberg/smarttrashpickerapp/HomeActivity.kt
@@ -131,6 +131,8 @@ class HomeActivity : AppCompatActivity() {
 
     private fun requestLocationSettings() {
         val locationRequest = LocationRequest.create()!!.apply {
+            interval = 10000
+            fastestInterval = 5000
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
 
